23ac992c9c5a9664ba0265da28c0a2b5b48ded31
Sir3eBpA <nazmouse22@hotmail.com>
2022-02-15 01:04:16 -0500

    Set up project

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using AdonisUI.Controls;
+
+namespace FFS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : AdonisWindow
+    {
+        public MainWindow()
+        {
+            
+        }
+    }
+}
