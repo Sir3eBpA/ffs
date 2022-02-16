7c94e991cd8ba95914b70d1b301ed81d7da0ce8f
Nazar Derevetskyy <nazmouse22@hotmail.com>
2022-02-15 22:00:22 -0500

    Hook up DI and Logger

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using FFS.Services.FileSystemScanner;
+using FFS.Services.FileSystemScanner.NTFS;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 namespace FFS
 {
@@ -13,5 +17,29 @@ namespace FFS
     /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            SetupDI();
+            SetupLogger();
+        }
+
+        private void SetupDI()
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<IFSScanner, NTFSScannerService>();
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void SetupLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
     }
 }
